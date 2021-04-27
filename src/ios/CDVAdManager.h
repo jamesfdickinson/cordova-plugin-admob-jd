94075589615ce58ebf5ff3cae54f6d0d9aad733a
Jimmy Dickinson <jamesfdickinson@gmail.com>
2021-04-27 02:57:53 -0700

    ios upgrade to sdk 8.4

--- src/ios/CDVAdManager.h
+++ src/ios/CDVAdManager.h
@@ -4,67 +4,41 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#import <GoogleMobileAds/GADAdSize.h>
-#import <GoogleMobileAds/GADBannerView.h>
-#import <GoogleMobileAds/GADInterstitial.h>
-#import <GoogleMobileAds/GADBannerViewDelegate.h>
-#import <GoogleMobileAds/GADInterstitialDelegate.h>
-#import <GoogleMobileAds/GADRewardedAdDelegate.h>
 #import <GoogleMobileAds/GoogleMobileAds.h>
 #import "CDVAdManagerAdListener.h"
 
 
 #pragma mark - JS requestAd options
 
-@class GADBannerView;
 @class GADInterstitial;
 @class GADRewardAd;
-@class CDVAdManagerAdListener;
 @class GADAppOpenAd;
 
 #pragma mark AdManager Plugin
 
-@interface CDVAdManager : CDVPlugin {
+@interface CDVAdManager : CDVPlugin <GADFullScreenContentDelegate> {
 }
 
 @property (assign) BOOL isInterstitialAvailable;
 @property (assign) BOOL isRewardedAvailable;
 @property (assign) BOOL isAppOpenAvailable;
 
-@property (nonatomic, retain) GADBannerView *bannerView;
 @property (nonatomic, retain) GADInterstitial *interstitialView;
 @property (nonatomic, retain) GADRewardedAd *rewardedView;
 @property (nonatomic, retain) GADAppOpenAd* appOpenAd;
-@property (nonatomic, retain) CDVAdManagerAdListener *adsListener;
 
-@property (nonatomic, retain) NSString* bannerAdId;
+
 @property (nonatomic, retain) NSString* interstitialAdId;
 @property (nonatomic, retain) NSString* rewardedAdId;
 @property (nonatomic, retain) NSString* appOpenAdId;
-@property (nonatomic, retain) NSString* tappxId;
 
-@property (assign) GADAdSize adSize;
-@property (assign) BOOL isBannerAtTop;
-@property (assign) BOOL isBannerOverlap;
-@property (assign) BOOL isOffsetStatusBar;
-
-@property (assign) BOOL isTesting;
 @property (nonatomic, retain) NSDictionary* adExtras;
 
-@property (assign) BOOL isBannerVisible;
-@property (assign) BOOL isBannerInitialized;
-@property (assign) BOOL isBannerShow;
-@property (assign) BOOL isBannerAutoShow;
 @property (assign) BOOL isInterstitialAutoShow;
 @property (assign) BOOL isRewardedAutoShow;
-@property (assign) BOOL hasTappx;
-@property (assign) double tappxShare;
+
 
 - (void)setOptions:(CDVInvokedUrlCommand *)command;
-
-- (void)createBannerView:(CDVInvokedUrlCommand *)command;
-- (void)showBannerAd:(CDVInvokedUrlCommand *)command;
-- (void)destroyBannerView:(CDVInvokedUrlCommand *)command;
 
 - (void)requestInterstitialAd:(CDVInvokedUrlCommand *)command;
 - (void)showInterstitialAd:(CDVInvokedUrlCommand *)command;
@@ -75,9 +49,8 @@
 - (void)requestAppOpenAd:(CDVInvokedUrlCommand *)command;
 - (void)showAppOpenAd:(CDVInvokedUrlCommand *)command;
 
-- (void)onBannerAd:(GADBannerView *)adView adListener:(CDVAdManagerAdListener *)adListener ;
-- (void)onInterstitialAd:(GADInterstitial *)interstitial adListener:(CDVAdManagerAdListener *)adListener;
-- (void)onRewardedAd:(GADRewardedAd *)rewarded adListener:(CDVAdManagerAdListener *)adListener;
-- (void)onAppOpenAd:(GADAppOpenAd *)appOpenAd adListener:(CDVAdManagerAdListener *)adListener;
+- (void)onInterstitialAd:(GADInterstitial *)interstitial;
+- (void)onRewardedAd:(GADRewardedAd *)rewarded ;
+- (void)onAppOpenAd:(GADAppOpenAd *)appOpenAd ;
 
 @end
